15a974c412d44c0c51330ccdab91283630826a61
Neil Sawbridge <nsawbridge@gmail.com>
2022-03-17 09:20:42 +0100

    Grid class added for all pump gauges

--- ibm-pumpdemo/src/child.js
+++ ibm-pumpdemo/src/child.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "carbon-components/css/carbon-components.min.css";
+import { Toggle } from "carbon-components-react";
 import TempGauge from "./TempGauge";
 import FlowGauge_1 from "./FlowGauge_1";
 import FlowGauge_2 from "./FlowGauge_2";
@@ -44,12 +46,20 @@ class ChildComponent extends Component {
 
         return (
             <div id="heading">
-                <h2>Pump: {this.props.pumpState}Valve: {this.props.valveState}Fan: {this.props.fanState}</h2>
+                <h2>Pump: {this.props.pumpState}</h2>
+                <Toggle
+                    aria-label="toggle button"
+                    defaultToggled
+                    id="toggle-1"
+                    labelText="Pump"
+                />
+                <h2>Valve: {this.props.valveState}</h2>
+                <h2>Fan: {this.props.fanState}</h2>
                 <br></br>
                 <div class="grid-container">
                     <div class="grid-item"><TempGauge temperature = {this.props.temp}/></div>
                     <div class="grid-item"><FanSpeed fanspeed = {this.props.fanSpeed}/></div>
-                    <div class="grid-item"><PumpSpeed pumpSpeed = {this.props.pumpSpeed}/></div>
+                    <div class="grid-item"><PumpSpeed pumpspeed = {this.props.pumpSpeed}/></div>
                     <div class="grid-item"><FlowGauge_1 flow = {this.props.flow1}/></div>
                     <div class="grid-item"><FlowGauge_2 flow = {this.props.flow2}/></div>
                     <div class="grid-item"><FlowGauge_3 flow = {this.props.flow3}/></div>
