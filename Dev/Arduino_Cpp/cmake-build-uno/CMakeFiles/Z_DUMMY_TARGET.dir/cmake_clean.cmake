02145b9e368c316f9f7a46696f3e92382137c51f
Robin Ellingsen <70570646+ascoolarobban@users.noreply.github.com>
2022-04-04 15:02:40 +0200

    New Arduino CPP

---
+++ Dev/Arduino_Cpp/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/Adafruit_Unified_Sensor/Adafruit_Sensor.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/DHT/DHT.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/breatheButton.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/buttonIsTriggered.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/commandHandler.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/debounce.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/detect_change.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/pumpControl.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/pumpSerialCommand.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/readButtons.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/readPotentiometers.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/runTime.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/sendJson.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/sensorReadings.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/setPWM.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/welcomeBlink.cpp.obj"
+  "Z_DUMMY_TARGET"
+  "Z_DUMMY_TARGET.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
